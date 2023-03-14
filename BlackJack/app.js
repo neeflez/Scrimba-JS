0b845c6cd1c5460cdd42187e73f6d1d8f94213a0
neeflez <nflzpl@gmail.com>
2023-03-14 01:47:52 +0100

    added rules display

---
+++ BlackJack/app.js
@@ -0,0 +1,9 @@
+function rules() {
+  let rules = document.getElementById("hero-bottom");
+  if (rules.style.display === "none") {
+    rules.style.display = "block";
+  }
+  else{
+    rules.style.display = "none";
+  }
+}
